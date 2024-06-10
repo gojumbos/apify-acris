58c0e377bbb154022bb66770bf30d075de200614
gojumbos <declan.borcich@tufts.edu>
2024-06-10 14:38:30 -0400

    add no sandbbox, disable dev shm, window size ...

--- src/browser.py
+++ src/browser.py
@@ -71,18 +71,18 @@ class Browser:
         chrome_options = ChromeOptions()
         # if Actor.config.headless:  # drb
         # chrome_options.add_argument('--headless')
-        # chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         # # DRB 6/6
-        # chrome_options.add_argument("--window-size=1920,1080")
-        # chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-extensions")
         # chrome_options.add_argument("--proxy-server='direct://'")
         # chrome_options.add_argument("--proxy-bypass-list=*")
         # chrome_options.add_argument("--start-maximized")
         # chrome_options.add_argument('--headless')
         # chrome_options.add_argument('--disable-gpu')
-        # chrome_options.add_argument('--ignore-certificate-errors')
-        # chrome_options.add_argument('--allow-running-insecure-content')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--allow-running-insecure-content')
         # user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
         chrome_options.add_argument(f'user-agent={user_agent}')
